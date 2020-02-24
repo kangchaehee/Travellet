e68126a0d92d616b2868818abaf41899e6108284
kangchaehee <rkdcogml0430@naver.com>
2020-02-25 00:57:32 +0900

    example

--- app/src/main/java/com/example/example/RegisterActivity.java
+++ app/src/main/java/com/example/example/RegisterActivity.java
@@ -1,10 +1,15 @@
 package com.example.example;
 
 import androidx.appcompat.app.AppCompatActivity;
-
+import android.widget.Spinner;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.AdapterView.OnItemSelectedListener;
+import android.widget.Toast;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.TextView;
 
 public class RegisterActivity extends AppCompatActivity {
 
@@ -12,7 +17,29 @@ public class RegisterActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
+
+        Spinner spinner = (Spinner) findViewById(R.id.spinner);
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(
+                this, R.array.array_country, android.R.layout.simple_spinner_item);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinner.setAdapter(adapter);
+
+        spinner.setOnItemSelectedListener(new MyOnItemSelectedListener());
     }
+
+    public class MyOnItemSelectedListener implements OnItemSelectedListener{
+        @Override
+        public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+            Toast.makeText(parent.getContext(),
+                    "Your country is "+parent.getItemAtPosition(position),
+                    Toast.LENGTH_SHORT).show();
+        }
+        @Override
+        public void onNothingSelected(AdapterView<?> arg0) {
+            // Do nothing
+        }
+    }
+
     public void onClicked(View view){
         Intent intent = new Intent(this, SignIn.class);
         startActivity(intent);
