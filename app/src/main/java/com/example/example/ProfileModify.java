93ff95f6701b58cbe0abaf098bc3fb125adefd77
kangchaehee <rkdcogml0430@naver.com>
2020-02-25 20:46:03 +0900

    example

--- app/src/main/java/com/example/example/ProfileModify.java
+++ app/src/main/java/com/example/example/ProfileModify.java
@@ -2,7 +2,9 @@ package com.example.example;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class ProfileModify extends AppCompatActivity {
 
@@ -10,5 +12,11 @@ public class ProfileModify extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profile_modify);
+    }
+
+    public void onClick(View view){
+
+        Intent intent = new Intent(this, Profile.class);
+        startActivity(intent);
     }
 }
