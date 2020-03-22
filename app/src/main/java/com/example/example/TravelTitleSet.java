86bb65ca35ec4e549a122ef619e3b6ede498637c
kangchaehee <rkdcogml0430@naver.com>
2020-03-22 18:40:21 +0900

    Revert "."
    This reverts commit 217b706b

--- app/src/main/java/com/example/example/TravelTitleSet.java
+++ app/src/main/java/com/example/example/TravelTitleSet.java
@@ -9,30 +9,35 @@ import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
 public class TravelTitleSet extends AppCompatActivity {
 
-    EditText edittitle;
+    EditText Edittext_title;
     Button btn_Next;
     String title;
-
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_travel_title_set);
 
-        edittitle = (EditText) findViewById(R.id.edittitle);
+        Edittext_title = (EditText) findViewById(R.id.Edittext_title);
 
         btn_Next = (Button) findViewById(R.id.btn_Next);
         btn_Next.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                if(title == null){
-                    edittitle.setHintTextColor(getColor(R.color.coral_red));
+                String title = Edittext_title.getText().toString();
+                if(title == null || title.length() == 0){
+                    Edittext_title.setHintTextColor(getColor(R.color.coral_red));
+                    //btn_Next.isClickable(false);
+                }else{
+                    Intent intent = new Intent(TravelTitleSet.this, TravelCalendar.class);
+                    startActivity(intent);
+                    overridePendingTransition(0, 0);
                 }
-                else
-                    returnToBack();
+
             }
         });
 
@@ -42,30 +47,41 @@ public class TravelTitleSet extends AppCompatActivity {
     protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent intent) {
         super.onActivityResult(requestCode, resultCode, intent);
 
-        if(requestCode == 1){
-            if(intent != null){
-                title = intent.getStringExtra("edittitle");
-                edittitle.setTextColor(getColor(R.color.soft_black));
-                edittitle.setText(title);
+        if(requestCode == 50){
+            String title = Edittext_title.getText().toString();
+            if(title != null){
+                //title2 = intent.getStringExtra("Edittext_title");
+                Edittext_title.setTextColor(getColor(R.color.soft_black));
+                Edittext_title.setText(title);
             }
         }
     }
 
     public void returnToBack() {
-
-        title = edittitle.getText().toString();
+        title = Edittext_title.getText().toString();
         if (title == null) {
             Log.d("null", "null");
         } else
-            Log.d("title", title);
+            Log.d("memo", title);
 
-        }
 
-    //Main Empty
-    public void onButtonClick(View view1){
+        Intent intent = new Intent();
+
+        setResult(RESULT_OK, intent);
         finish();
     }
 
+
+
+    //Main Empty
+    public void onButtonClick(View view1){
+
+        Intent intent = new Intent(TravelTitleSet.this, MainActivity.class);
+        startActivity(intent);
+        overridePendingTransition(0, 0);
+    }
+
+    /*
     //travelcalendar
     public void onClick(View view){
 
@@ -73,4 +89,6 @@ public class TravelTitleSet extends AppCompatActivity {
         startActivity(intent);
         overridePendingTransition(0, 0);
     }
+
+     */
 }
