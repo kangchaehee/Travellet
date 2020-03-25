84b99f698ab894e4455f9a82d31cadf03e227699
kangchaehee <rkdcogml0430@naver.com>
2020-03-25 21:22:12 +0900

    Revert "."
    This reverts commit 217b706b

--- app/src/main/java/com/example/example/MainActivity.java
+++ app/src/main/java/com/example/example/MainActivity.java
@@ -2,33 +2,58 @@ package com.example.example;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.graphics.Color;
 import android.graphics.PorterDuff;
 import android.os.Bundle;
 import android.view.View;
+import android.view.ViewGroup;
+import android.view.WindowManager;
+import android.widget.BaseAdapter;
 import android.widget.Button;
 import android.widget.ImageButton;
+import android.widget.ImageView;
+import android.widget.ListView;
+import android.widget.TextView;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
 
     MainActivityUpcomingFragment fragment1;
     MainActivityPastFragment fragment2;
+
     ImageButton up_delete, past_delete;
     Button btn_upcoming, btn_past;
 
     boolean upcomingState=false, pastState=false;
-    //DeleteDialog oDialog;
+/*
+    DeleteDialog oDialog;
+
+    ListView listView;
+    ArrayList<MainActivitySubItem> items = new ArrayList<MainActivitySubItem>();
+    MainSubAdapter adapter = new MainSubAdapter();
 
     //ArrayList<PastFragment> items = new ArrayList<PastFragment>();
-    //ArrayList<UpcomingFragment> item = new ArrayList<UpcommingFragment>();
-    //MainActivity.PlanSubAdapter adapter = new PlanInitialActivity.PlanSubAdapter();
+    //ArrayList<UpcomingFragment> item = new ArrayList<UpcomingFragment>();
 
+ */
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        /*
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        setContentView(R.layout.activity_plan_initial);
+
+        listView = (ListView) findViewById(R.id.container);
+        listView.setAdapter(adapter);
+
+         */
 
         fragment1 = new MainActivityUpcomingFragment();
         fragment2 = new MainActivityPastFragment();
@@ -78,60 +103,11 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
+        //oDialog = new DeleteDialog(this);
+
     }
 
-    /*class PlanSubAdapter extends BaseAdapter {
 
-        @Override
-        public int getCount() {
-            return items.size();
-        }
-
-        public void addItem(PlanInitialSubItem item) {
-            items.add(item);
-        }
-
-        @Override
-        public Object getItem(int position) {
-            return items.get(position);
-        }
-
-        @Override
-        public long getItemId(int position) {
-            return position;
-        }
-
-        @Override
-        public View getView(final int position, View convertView, ViewGroup parent) {
-            PlanInitialSubItemView view= new PlanInitialSubItemView(getApplicationContext());
-            final PlanInitialSubItem item = items.get(position);
-            view.setPlaceTime(item.getPlaceTime());
-            view.setPlaceName(item.getPlaceName());
-            view.setPlaceMemo(item.getPlaceMemo());
-
-            ImageButton del = (ImageButton) view.findViewById(R.id.deleteButton);
-            del.setOnClickListener(new View.OnClickListener() {
-                @Override
-                public void onClick(View v) {
-                    oDialog.setCancelable(false);
-                    oDialog.show();
-
-                    oDialog.setOnDismissListener(new DialogInterface.OnDismissListener() {
-                        @Override
-                        public void onDismiss(DialogInterface dialog) {
-                            boolean del = oDialog.getDelete();
-                            if(del){
-                                items.remove(position);
-                                adapter.notifyDataSetChanged();
-                            }
-                        }
-                    });
-
-                }
-            });
-            return view;
-        }
-    }*/
 
     //travel title set
     public void onClick(View view1){
