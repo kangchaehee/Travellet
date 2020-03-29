133a743ab85457a03380c527d0b20cb5fb4b09c8
kangchaehee <rkdcogml0430@naver.com>
2020-03-29 16:03:50 +0900

    Revert "."
    This reverts commit 217b706b

--- app/src/main/java/com/example/example/TravelCalendar.java
+++ app/src/main/java/com/example/example/TravelCalendar.java
@@ -1,139 +1,117 @@
 package com.example.example;
 
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.annotation.IdRes;
-import androidx.annotation.Nullable;
 import android.content.Intent;
-import android.graphics.Color;
 import android.os.Bundle;
-import android.view.MenuInflater;
-import android.view.View;
+import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 import android.view.Menu;
-import androidx.recyclerview.widget.OrientationHelper;
 import android.view.MenuItem;
-import android.widget.RadioGroup;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageButton;
+import android.widget.TextView;
 import android.widget.Toast;
+import com.yongbeom.aircalendar.AirCalendarDatePickerActivity;
+import com.yongbeom.aircalendar.core.AirCalendarIntent;
 import java.util.ArrayList;
 import java.util.Calendar;
 import java.util.List;
-import com.applikeysolutions.cosmocalendar.utils.SelectionType;
-import com.applikeysolutions.cosmocalendar.view.CalendarView;
-import java.text.SimpleDateFormat;
-import java.util.Calendar;
-import java.util.List;
 
-public class TravelCalendar extends AppCompatActivity implements RadioGroup.OnCheckedChangeListener{
+public class TravelCalendar extends AppCompatActivity {
+    public final static int REQUEST_CODE = 1;
 
-    private CalendarView calendarView;
+    //static ImageButton btn_back;
+    //static TextView tv_done_btn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_travel_calendar);
 
-        setSupportActionBar((Toolbar) findViewById(R.id.toolbar));
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
 
-        initViews();
+        //btn_back = (ImageButton) findViewById(com.yongbeom.aircalendar.R.id.btn_back);
+        //tv_done_btn = (TextView) findViewById(R.id.rl_done_btn);
+
+        AirCalendarIntent intent = new AirCalendarIntent(this);
+        intent.setSelectButtonText("Select");
+        intent.setResetBtnText("Reset");
+        intent.setWeekStart(Calendar.MONDAY);
+
+        /*
+        tv_done_btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            Intent intent = new Intent(TravelCalendar.this, MainActivity.class);
+                startActivity(intent);
+            }
+        });
+
+         */
+
+//        ArrayList<String> weekDay = new ArrayList<>();
+//        weekDay.add("M");
+//        weekDay.add("T");
+//        weekDay.add("W");
+//        weekDay.add("T");
+//        weekDay.add("F");
+//        weekDay.add("S");
+//        weekDay.add("S");
+//        intent.setCustomWeekDays(weekDay);
+//        intent.setWeekDaysLanguage(AirCalendarIntent.Language.EN);
+//        intent.isSingleSelect(false);
+//        intent.isBooking(false); // DEFAULT false
+//        intent.isSelect(true); // DEFAULT false
+//        intent.setBookingDateArray();
+//        intent.setStartDate(2017, 12, 1);
+//        intent.setEndDate(2017, 12, 1);
+//        intent.isMonthLabels(false);
+//        intent.setActiveMonth(3);
+//        intent.setStartYear(2018);
+//        intent.setMaxYear(2030);
+        startActivityForResult(intent, REQUEST_CODE);
     }
 
-    private void initViews() {
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
 
-        calendarView = (CalendarView) findViewById(R.id.calendar_view);
-        calendarView.setCalendarOrientation(OrientationHelper.HORIZONTAL);
-        ((RadioGroup) findViewById(R.id.rg_selection_type)).setOnCheckedChangeListener((RadioGroup.OnCheckedChangeListener) this);
+        if (resultCode == RESULT_OK && requestCode == REQUEST_CODE) {
+//            AirCalendarDatePickerActivity.RESULT_SELECT_START_DATE
+//            AirCalendarDatePickerActivity.RESULT_SELECT_END_DATE
+//            AirCalendarDatePickerActivity.RESULT_SELECT_START_VIEW_DATE
+//            AirCalendarDatePickerActivity.RESULT_SELECT_END_VIEW_DATE
+//            AirCalendarDatePickerActivity.RESULT_FLAG
+//            AirCalendarDatePickerActivity.RESULT_TYPE
+//            AirCalendarDatePickerActivity.RESULT_STATE
+
+            if(data != null){
+                Toast.makeText(this, "Select Date range : " + data.getStringExtra(AirCalendarDatePickerActivity.RESULT_SELECT_START_DATE) + " ~ " + data.getStringExtra(AirCalendarDatePickerActivity.RESULT_SELECT_END_DATE), Toast.LENGTH_SHORT).show();
+            }
+        }
     }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
-        MenuInflater inflater = getMenuInflater();
-        inflater.inflate(R.menu.menu, menu);
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
         return true;
     }
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
-        switch (item.getItemId()) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
 
-            case R.id.clear_selections:
-                clearSelectionsMenuClick();
-                return true;
-
-            case R.id.show_selections:
-                List<Calendar> days = calendarView.getSelectedDates();
-
-                String result="";
-                for( int i=0; i<days.size(); i++)
-                {
-                    Calendar calendar = days.get(i);
-                    final int day = calendar.get(Calendar.DAY_OF_MONTH);
-                    final int month = calendar.get(Calendar.MONTH);
-                    final int year = calendar.get(Calendar.YEAR);
-                    String week = new SimpleDateFormat("EE").format(calendar.getTime());
-                    String day_full = week +"day" + ". " + (month+1) + ". " + day + ". " + year;
-                    //String day_full = year + "년 "+ (month+1)  + "월 " + day + "일 " + week + "요일";
-                    result += (day_full + "\n");
-                }
-                    //토스트 메세지 -> 없애장
-                Toast.makeText(TravelCalendar.this, result, Toast.LENGTH_LONG).show();
-                return true;
-
-            default:
-                return super.onOptionsItemSelected(item);
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
         }
+
+        return super.onOptionsItemSelected(item);
     }
-
-    private void clearSelectionsMenuClick() {
-        calendarView.clearSelections();
-    }
-
-
-    public void onCheckedChanged(RadioGroup group, @IdRes int checkedId) {
-        clearSelectionsMenuClick();
-        switch (checkedId) {
-
-            /*
-            case R.id.rb_single:
-                calendarView.setSelectionType(SelectionType.SINGLE);
-                break;
-            case R.id.rb_multiple:
-                calendarView.setSelectionType(SelectionType.MULTIPLE);
-                break;
-             */
-
-            case R.id.rb_range:
-                calendarView.setSelectionType(SelectionType.RANGE);
-                break;
-
-                /*
-            case R.id.rb_none:
-                calendarView.setSelectionType(SelectionType.NONE);
-                break;
-                 */
-        }
-    }
-
-        //preference set
-    public void onButtonClick(View view1){
-
-        Intent intent = new Intent(this, TravelPreferenceSet.class);
-        startActivity(intent);
-        overridePendingTransition(0, 0);
-    }
-
-        // Budget show
-    public void onClick(View view){
-
-        Intent intent = new Intent(this, TravelBudgetShow.class);
-        startActivity(intent);
-        overridePendingTransition(0, 0);
-    }
-
-    @Override
-    public void onBackPressed() {
-        super.onBackPressed();
-        finish();
-        overridePendingTransition(0, 0);
-    }
-
 }
