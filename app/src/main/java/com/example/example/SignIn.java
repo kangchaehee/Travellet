1f5375ac882df18537e5576b6223dac08efe9f94
Jiseondf <48876106+Jiseondf@users.noreply.github.com>
2020-04-02 01:55:25 +0900

    뀨잇

--- app/src/main/java/com/example/example/SignIn.java
+++ app/src/main/java/com/example/example/SignIn.java
@@ -178,7 +178,6 @@ public class SignIn extends AppCompatActivity {
         //register 버튼. 누르면 register로
     public void onButtonClick(View view2){
         Intent intent = new Intent(this, RegisterActivity.class);
-        intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
         startActivity(intent);
     }
 }
