8cc9d03d86082cc6aeb0008b374fd0b2d61d416e
kangchaehee <rkdcogml0430@naver.com>
2020-04-02 22:20:39 +0900

    bar chart 오류있음

--- app/src/main/java/com/example/example/ReportDaily.java
+++ app/src/main/java/com/example/example/ReportDaily.java
@@ -4,6 +4,7 @@ import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
+import androidx.room.Index;
 
 import android.os.Bundle;
 import android.view.LayoutInflater;
@@ -28,60 +29,86 @@ import com.github.mikephil.charting.highlight.Highlight;
 import com.github.mikephil.charting.listener.OnChartValueSelectedListener;
 import com.github.mikephil.charting.utils.ColorTemplate;
 
+import com.github.mikephil.charting.interfaces.datasets.IBarDataSet;
+import com.github.mikephil.charting.formatter.ValueFormatter;
+//import com.github.mikephil.charting.utils.PercentFormatter;
+
 import java.util.ArrayList;
 import java.util.List;
+
+import lecho.lib.hellocharts.formatter.AxisValueFormatter;
 
 
 public class ReportDaily extends Fragment {
 
     BarChart chart;
-    //float barSpace, barWidth, groupSpace;
+    float barSpace, barWidth, groupSpace;
+    String[] days;
+
 
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         ViewGroup rootView = (ViewGroup) inflater.inflate(R.layout.activity_report_daily, container, false);
 
-        BarChart chart = rootView.findViewById(R.id.barchart);
-
-        ArrayList<BarEntry> entries = new ArrayList<>();
-        entries.add(new BarEntry(100f, 0));
-        entries.add(new BarEntry(150f, 1));
-        entries.add(new BarEntry(200f, 2));
-        entries.add(new BarEntry(500f, 3));
-        entries.add(new BarEntry(1000f, 4));
-        entries.add(new BarEntry(500f, 5));
-
-        BarDataSet barDataSet = new BarDataSet(entries, "Inducesmile");
-        //barDataSet.setBarBorderWidth(0.9f);
-
-        ArrayList<String> labels = new ArrayList<String>();
-        labels.add("Day1");
-        labels.add("Day2");
-        labels.add("Day3");
-        labels.add("Day4");
-        labels.add("Day5");
-        labels.add("Day6");
-
-        XAxis x = chart.getXAxis();
-        x.setTextColor(R.color.icon_grey);
-
-        YAxis y = chart.getAxisLeft();
-        y.setTextColor(R.color.soft_grey);
-
-        Legend legend = chart.getLegend();
-        legend.setTextColor(R.color.soft_grey);
-        legend.setEnabled(false);
-
-
-        BarData data = new BarData(labels, barDataSet);
-        chart.setData(data); // set the data and list of labels into chart
+        BarChart Chart = (BarChart) rootView.findViewById(R.id.barchart);
+        chart.setDrawBarShadow(false);
+        chart.setDrawValueAboveBar(true);
         chart.setDescription("");
+        chart.setMaxVisibleValueCount(50);
+        chart.setPinchZoom(false);
+        chart.setDrawGridBackground(false);
 
+        ArrayList<BarEntry> entries1 = new ArrayList<>();
+        ArrayList<BarEntry> entries2 = new ArrayList<>();
 
+        entries1.add(new BarEntry(100f, 0));
+        entries1.add(new BarEntry(150f, 1));
+        entries1.add(new BarEntry(200f, 2));
+        entries1.add(new BarEntry(500f, 3));
+        entries1.add(new BarEntry(1000f, 4));
+        entries1.add(new BarEntry(500f, 5));
 
-        barDataSet.setColors(ColorTemplate.COLORFUL_COLORS);
+        entries2.add(new BarEntry(1400f, 0));
+        entries2.add(new BarEntry(100f, 1));
+        entries2.add(new BarEntry(300f, 2));
+        entries2.add(new BarEntry(200f, 3));
+        entries2.add(new BarEntry(800f, 4));
+        entries2.add(new BarEntry(600f, 5));
 
+        BarDataSet barDataSet1 = new BarDataSet(entries1, "cash");
+        barDataSet1.setColor(R.color.light_blue);
+
+        BarDataSet barDataSet2 = new BarDataSet(entries2, "card");
+        barDataSet2.setColor(R.color.blue);
+
+        BarData data = new BarData(barDataSet1, barDataSet2);
+        chart.setData(data);
+        data.setValueFormatter(new LargeValueFormatter());
+
+        barWidth = 0.3f;
+        barSpace = 0f;
+        groupSpace = 0.4f;
+
+        chart.setData(data);
+        chart.getBarData().setBarWidth(barWidth);
+        chart.getXAxis().setAxisMinimum(0);
+        chart.getXAxis().setAxisMaximum(0 + chart.getBarData().getGroupWidth(groupSpace, barSpace) * groupCount);
+        chart.groupBars(0, groupSpace, barSpace);
+        chart.getData().setHighlightEnabled(false);
+        chart.invalidate();
+
+        /*
+        Legend l = chart.getLegend();
+        l.setVerticalAlignment(Legend.LegendVerticalAlignment.TOP);
+        l.setHorizontalAlignment(Legend.LegendHorizontalAlignment.RIGHT);
+        l.setOrientation(Legend.LegendOrientation.HORIZONTAL);
+        l.setDrawInside(true);
+        l.setYOffset(20f);
+        l.setXOffset(0f);
+        l.setYEntrySpace(0f);
+        l.setTextSize(8f);
+        */
         return rootView;
     }
 }
