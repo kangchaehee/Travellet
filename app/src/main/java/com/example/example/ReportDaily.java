459c80539917ba43b88985283192fdf55bea8932
kangchaehee <rkdcogml0430@naver.com>
2020-04-03 18:41:27 +0900

    ver.4

--- app/src/main/java/com/example/example/ReportDaily.java
+++ app/src/main/java/com/example/example/ReportDaily.java
@@ -42,105 +42,39 @@ import lecho.lib.hellocharts.formatter.AxisValueFormatter;
 public class ReportDaily extends Fragment {
 
     BarChart chart;
-    float barSpace, barWidth, groupSpace;
-    String[] days;
-
+    //float barSpace, barWidth, groupSpace;
+    //String[] days;
 
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        ViewGroup rootView = (ViewGroup) inflater.inflate(R.layout.activity_report_daily, container, false);
+        View view = inflater.inflate(R.layout.activity_report_daily, null);
+        initView(view);
+        initChart();
+        return view;
+    }
 
-        BarChart Chart = (BarChart) rootView.findViewById(R.id.barchart);
+    private void initView(View view) {
+        chart = (BarChart) view.findViewById(R.id.barchart);
+    }
+    private void initChart(){
+        List<BarEntry> yvalue=new ArrayList<>();
 
-        barWidth = 0.3f;
-        barSpace = 0f;
-        groupSpace = 0.4f;
+        yvalue.add(new BarEntry(new float[]{123,455},0));
+        yvalue.add(new BarEntry(new float[]{35,325},1));
+        yvalue.add(new BarEntry(new float[]{12,95},2));
+        yvalue.add(new BarEntry(new float[]{122,195},3));
+        BarDataSet set=new BarDataSet(yvalue,"");
+        set.setColors(new int[]{R.color.light_blue, R.color.blue});
 
-        BarData data = new BarData(xAxis(), dataSets());
+        List<String> xvalue=new ArrayList<>();
+        xvalue.add("Day1");
+        xvalue.add("Day2");
+        xvalue.add("Day3");
+        xvalue.add("Day4");
 
-        chart.getBarData();
+        BarData data=new BarData(xvalue,set);
+
         chart.setData(data);
-        chart.setDrawBarShadow(false);
-        chart.setDrawValueAboveBar(true);
-        chart.setDescription("");
-        chart.setMaxVisibleValueCount(50);
-        chart.setPinchZoom(false);
-        chart.setDrawGridBackground(false);
-        data.setValueFormatter(new LargeValueFormatter());
-
-        //chart.getBarData().setBarWidth(barWidth);
-        chart.getXAxis().setAxisMinValue(0);
-        //chart.getXAxis().setAxisMaxValue(0 + chart.getBarData().getGroupWidth(groupSpace, barSpace) * groupCount);
-        //chart.getgroupBars(0, groupSpace, barSpace);
-        chart.getData().setHighlightEnabled(false);
-
-        /*
-        Legend l = chart.getLegend();
-        l.setVerticalAlignment(Legend.LegendVerticalAlignment.TOP);
-        l.setHorizontalAlignment(Legend.LegendHorizontalAlignment.RIGHT);
-        l.setOrientation(Legend.LegendOrientation.HORIZONTAL);
-        l.setDrawInside(true);
-        l.setYOffset(20f);
-        l.setXOffset(0f);
-        l.setYEntrySpace(0f);
-        l.setTextSize(8f);
-        */
-        return rootView;
     }
-
-    private List<IBarDataSet> dataSets() {
-        ArrayList<BarDataSet> dataSets = null;
-        ArrayList<BarEntry> valueSet1 = new ArrayList<>();
-        BarEntry v1e1 = new BarEntry(110.000f, 0); // Jan
-        valueSet1.add(v1e1);
-        BarEntry v1e2 = new BarEntry(40.000f, 1); // Feb
-        valueSet1.add(v1e2);
-        BarEntry v1e3 = new BarEntry(60.000f, 2); // Mar
-        valueSet1.add(v1e3);
-        BarEntry v1e4 = new BarEntry(30.000f, 3); // Apr
-        valueSet1.add(v1e4);
-        BarEntry v1e5 = new BarEntry(90.000f, 4); // May
-        valueSet1.add(v1e5);
-        BarEntry v1e6 = new BarEntry(100.000f, 5); // Jun
-        valueSet1.add(v1e6);
-
-        ArrayList<BarEntry> valueSet2 = new ArrayList<>();
-        BarEntry v2e1 = new BarEntry(150.000f, 0); // Jan
-        valueSet2.add(v2e1);
-        BarEntry v2e2 = new BarEntry(90.000f, 1); // Feb
-        valueSet2.add(v2e2);
-        BarEntry v2e3 = new BarEntry(120.000f, 2); // Mar
-        valueSet2.add(v2e3);
-        BarEntry v2e4 = new BarEntry(60.000f, 3); // Apr
-        valueSet2.add(v2e4);
-        BarEntry v2e5 = new BarEntry(20.000f, 4); // May
-        valueSet2.add(v2e5);
-        BarEntry v2e6 = new BarEntry(80.000f, 5); // Jun
-        valueSet2.add(v2e6);
-        BarDataSet barDataSet1 = new BarDataSet(valueSet1, "Brand 1");
-        barDataSet1.setColor(R.color.light_blue);
-        BarDataSet barDataSet2 = new BarDataSet(valueSet2, "Brand 2");
-        barDataSet2.setColor(R.color.blue);
-
-        dataSets = new ArrayList<>();
-        dataSets.add(barDataSet1);
-        dataSets.add(barDataSet2);
-
-        return dataSets();
-
-    }
-
-    private String[] xAxis() {
-        ArrayList<String> xAxis = new ArrayList<>();
-        xAxis.add("JAN");
-        xAxis.add("FEB");
-        xAxis.add("MAR");
-        xAxis.add("APR");
-        xAxis.add("MAY");
-        xAxis.add("JUN");
-        return xAxis();
-    }
-
-
 }
