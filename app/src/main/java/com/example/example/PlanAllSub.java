f97966b8f00dc0504edb4383c4afabdd3d74e7c5
Jiseondf <48876106+Jiseondf@users.noreply.github.com>
2020-04-14 17:44:42 +0900

    cost

--- app/src/main/java/com/example/example/PlanAllSub.java
+++ app/src/main/java/com/example/example/PlanAllSub.java
@@ -2,13 +2,41 @@ package com.example.example;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
 import android.os.Bundle;
+import android.util.AttributeSet;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.LinearLayout;
+import android.widget.ListView;
+import android.widget.TextView;
 
-public class PlanAllSub extends AppCompatActivity {
+public class PlanAllSub extends LinearLayout {
+    TextView day, period;
+    ListView listView;
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_plan_all_sub);
+    public PlanAllSub(Context context, AttributeSet attrs) {
+        super(context, attrs);
+
+        init(context);
     }
+
+    public PlanAllSub(Context context) {
+        super(context);
+
+        init(context);
+    }
+    private void init(Context context){
+        LayoutInflater inflater =(LayoutInflater)context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+        inflater.inflate(R.layout.activity_plan_all_sub, this, true);
+
+        day = findViewById(R.id.day);
+        period = findViewById(R.id.period);
+        listView = findViewById(R.id.con);
+    }
+
+    public void setDay(){
+
+    }
+
 }
