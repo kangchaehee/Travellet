b01ff3a212fef57a4c0fcedfd0325a9b0d944c3a
suyeon <koo5717@naver.com>
2020-04-15 17:54:17 +0900

    정리

--- app/src/main/java/com/example/example/network/AppHelper.java
+++ app/src/main/java/com/example/example/network/AppHelper.java
@@ -5,5 +5,5 @@ import com.android.volley.RequestQueue;
 public class AppHelper {
     public static RequestQueue requestQueue;
     public static String host = "https://openexchangerates.org/api/latest.json";
-    public static String appId = "7d3ff82d5d304cd6bf97c09581aa8bbf";
+    public static String appId = "";
 }
