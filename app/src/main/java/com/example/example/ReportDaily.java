b9c9a27cf05d4cde256bfb1465494adb9c9f3848
kangchaehee <rkdcogml0430@naver.com>
2020-05-27 00:45:30 +0900

    //

--- app/src/main/java/com/example/example/ReportDaily.java
+++ app/src/main/java/com/example/example/ReportDaily.java
@@ -11,6 +11,7 @@ import androidx.room.Index;
 
 import android.content.Context;
 import android.content.Intent;
+import android.database.sqlite.SQLiteDatabase;
 import android.graphics.Color;
 import android.graphics.Typeface;
 import android.os.Build;
@@ -64,13 +65,14 @@ public class ReportDaily extends Fragment {
     BarChart chart;
     float barSpace, barWidth, groupSpace;
     int groupCount;
-    int a, b, c, d, t, s;
+    int a, b, c, d, x, y, day;
     int startYear, startMonth, startDay, startDoW, endYear, endMonth, endDay, endDoW, period;
 
     LinearLayout linear8;
     HorizontalScrollView scroll;
 
-    TextView chartDay, chartMon, chartCard, chartCash;
+    TextView chartDay, chartMon;
+    SQLiteDatabase database;
 
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
@@ -80,18 +82,6 @@ public class ReportDaily extends Fragment {
 
         Typeface typeface = ResourcesCompat.getFont(getContext(), R.font.roboto_regular);
 
-        if(getArguments() != null){
-
-            startYear = getArguments().getInt("startYear", 0);
-            startDay = getArguments().getInt("startDay", 0);
-            startMonth = getArguments().getInt("startMonth", 0);
-            startDoW = getArguments().getInt("startDow", 0);
-            endYear = getArguments().getInt("endYear", 0);
-            endMonth = getArguments().getInt("endMonth", 0);
-            endDay = getArguments().getInt("endDay", 0);
-            endDoW = getArguments().getInt("endDow", 0);
-
-        }
         return view;
     }
 
@@ -99,14 +89,12 @@ public class ReportDaily extends Fragment {
         chart = (BarChart) view.findViewById(R.id.barchart);
         chartMon = (TextView) view.findViewById(R.id.chartMon);
         chartDay = (TextView) view.findViewById(R.id.chartDay);
-        //chartCard = (TextView) view.findViewById(R.id.chartCard);
-        //chartCash = (TextView) view.findViewById(R.id.chartCash);
 
     }
 
     private void initChart(){
 
-        int a = 4; // 여행 날짜 수 endDay - startDay?
+        int a = 4 ; // 여행 날짜 수 endDay - startDay
         List<String> xvalue=new ArrayList<>();
 
             // xvalue 에 day 넣는 거
@@ -164,7 +152,6 @@ public class ReportDaily extends Fragment {
         left.setTextColor(Color.parseColor("#c8cbd3")); // icon grey
         left.setTextSize(11f);
         left.setDrawZeroLine(true); // draw a zero line
-        //left.getValueFormatter();
 
         chart.getAxisRight().setEnabled(false); // no right axis
         chart.setScaleEnabled(false);
@@ -175,10 +162,10 @@ public class ReportDaily extends Fragment {
         chart.isClickable();
         //chart.setViewPortOffsets(0f, 0f, 0f, 0f); // day 랑 그래프 띄어져 있는 공간 넓이
 
-        float barWidth = 0.3f; //0.3
-        float groupSpace= 0.06f; //0.06
-        float barSpace = 0.0f; //0.0
-        int groupCount = 3; //3
+        float barWidth = 0.3f;
+        float groupSpace= 0.06f;
+        float barSpace = 0.0f;
+        int groupCount = 3;
 
         chart.getBarData().setGroupSpace(groupSpace);
         chart.getBarData().setGroupSpace(barSpace);
@@ -192,9 +179,24 @@ public class ReportDaily extends Fragment {
         chart.isHorizontalScrollBarEnabled();
         chart.setDoubleTapToZoomEnabled(false); // 줌인
         chart.setDrawValueAboveBar(true);
-        //chartMon.setText("130,000 ￦");
-        //chartDay.setText("Day 1");
-//
+
+
+        chart.setOnChartValueSelectedListener(new OnChartValueSelectedListener() {
+            @Override
+            public void onValueSelected(Entry e, int dataSetIndex, Highlight h) {
+
+                int t = 1;
+                int i = e.getXIndex(); //day 값
+                //chartMon.setText(String.valueOf(e.getVal()) + ",000" + " ￦" )
+                chartMon.setText(String.format("%.0f", e.getVal())+ ",000" + " ￦"); //소수점 없애는거
+                chartDay.setText("Day" + String.valueOf(e.getXIndex() + 1));
+            }
+
+            @Override
+            public void onNothingSelected() {
+            }
+        });
+
         /*
         chart.setOnChartValueSelectedListener(new OnChartValueSelectedListener() {
             @Override
@@ -213,49 +215,6 @@ public class ReportDaily extends Fragment {
                     //chartMon.setText(String.format("%.0f", e.getVal())+ ",000" + " ￦"); //소수점 없애는거
                     //chartDay.setText("day"+ i + t);
 
-
-                }
-
-
-                /*
-                chartDay.setText("Day 1");
-                switch (i){
-                    case 0:
-                        chartDay.setText("Day 1");
-                        break;
-                    case 1:
-                        chartDay.setText("Day 2");
-                        break;
-                    case 2:
-                        chartDay.setText("Day 3");
-                        break;
-                    case 3:
-                        chartDay.setText("Day 4");
-                        break;
-                    case 4:
-                        chartDay.setText("Day 5");
-                        break;
-                    case 5:
-                        chartDay.setText("Day 6");
-                        break;
-                    case 6:
-                        chartDay.setText("Day 7");
-                        break;
-                    case 7:
-                        chartDay.setText("Day 8");
-                        break;
-                    case 8:
-                        chartDay.setText("Day 9");
-                        break;
-                    case 9:
-                        chartDay.setText("Day 10");
-                        break;
-                    case 10:
-                        chartDay.setText("Day 11");
-                        break;
-                    case 11:
-                        chartDay.setText("Day 12");
-                        break;
                 }
             }
 
@@ -266,7 +225,6 @@ public class ReportDaily extends Fragment {
 
             }
         });
-
          */
     }
 }
